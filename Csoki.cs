5f5670945d7fbae5f7b008074cec67fd5d19923d
csoke.balazs <csoke.balazs@117-13>
2023-03-02 14:32:26 +0100

    Add project files.

---
+++ Csoki.cs
@@ -0,0 +1,19 @@
+﻿namespace HelloWorld
+{
+    internal class Csoki
+    {
+        public string Nev;
+        public string Kod;
+        public int Ar;
+        public int Darabszam;
+
+        public Csoki(string Sor)
+        {
+            var dbok = Sor.Split(';');
+            this.Nev = dbok[0];
+            this.Kod = dbok[1];
+            this.Ar = int.Parse(dbok[2]);
+            this.Darabszam = int.Parse(dbok[3]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Text;
+namespace HelloWorld
+{
+    class Program
+    {
+        static List<Csoki> Csoki_Lista = new List<Csoki>();
+        static void Main(string[] args)
+        {
+            Feladat1Beolvasa();
+            Feladat2();
+            Feladat3();
+        }
+
+        static void Feladat3()
+        {
+            Console.WriteLine("Feladat 3: Legdrágább");
+            int MaxAr = int.MaxValue;
+            string MaxCsoki = "cica";
+            foreach(var cs in Csoki_Lista)
+            {
+                if(MaxAr < cs.Ar)
+                {
+                    MaxCsoki = cs.Nev;
+                    MaxAr = cs.Ar;
+                }
+            }
+        }
+
+        static void Feladat2()
+        {
+            Console.WriteLine("Fealdat 2");
+            Console.WriteLine($"Beolvasott sorok száma: {Csoki_Lista.Count}");
+        }
+
+        static void Feladat1Beolvasa()
+        {
+            Console.WriteLine("Feladat 1: Beolvasás");
+            var sr = new StreamReader(@"csoki.txt", Encoding.UTF8);
+            int db = 0;
+            while (!sr.EndOfStream)
+            {
+                Csoki_Lista.Add(new Csoki(sr.ReadLine()));
+                db++;
+            }
+            sr.Close();
+            if (db > 0) { Console.WriteLine($"Sikeres beolvasás! {db}"); }
+            else { Console.WriteLine($"Sikertelen beolvasás"); }
+        }
+    }
